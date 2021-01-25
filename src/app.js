765eb5f615f2836b19987a1b3ae3def115708143
Justin Foster <jfoster91@gmail.com>
2021-01-25 16:57:02 -0700

    Board saves in server

--- src/app.js
+++ src/app.js
@@ -14,13 +14,72 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+app.use(express.json());
 
-// epStart = EndPointStart, this automatically changes the server test
-const epStart = '/';
-const startupGreet = 'Welcome aboard Captain, all systems online.';
-app.get(epStart, (req, res) => {
-  res.send(startupGreet)
+// let passItUp = "Hello Client, from Server"
+// app.get('/test', (req, res) => {
+//   res.send({message: passItUp})
+// })
+
+// app.post('/test', (req, res) => {
+
+//   console.log(req.body);
+//   res.send({message: "Message received:"})
+// })
+
+app.get('/play', (req, res) => {
+  // res.send({gameBoard: [['O',' ','O'],['X',' ','X'],['O',' ','O']]})
+  
+  req.app.get('db')('game_states')
+    .where({})
+    .then((gameStates) => {
+      res.send({gameBoard: JSON.parse(gameStates[0].game_board)})
+    })
 })
+
+app.post('/play', (req, res) => {
+  console.log(req.body);
+  req.app.get('db')('game_states')
+    .where({})
+    .then((gameStates) => {
+      // console.log(gameStates);
+
+      if (gameStates.length === 0) {
+        req.app.get('db')('game_states').insert({
+          game_board: JSON.stringify(req.body.gameBoard),
+          player_one_wins: req.body.playerOneWins,
+          player_two_wins: req.body.playerTwoWins,
+        })
+        .returning('*')
+        .then(game_state => {
+          res.send({message: "Message received"})
+        })
+      } else{
+        req.app.get('db')('game_states').where({id: gameStates[0].id}).update({
+          game_board: JSON.stringify(req.body.gameBoard),
+          player_one_wins: req.body.playerOneWins,
+          player_two_wins: req.body.playerTwoWins,
+        })
+        .returning('*')
+        .then(game_state => {
+          res.send({message: "Message received"})
+        })
+      }
+    })
+  
+
+
+  // return db('game_state')
+  //   .update(partType, partName)
+  //   .where({ id: shipId })
+  //   .returning('*')
+
+
+
+  
+})
+
+
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
@@ -33,4 +92,4 @@ app.use(function errorHandler(error, req, res, next) {
   res.status(500).json(response);
 });
 
-module.exports = { app, epStart, startupGreet }
+module.exports = { app }
